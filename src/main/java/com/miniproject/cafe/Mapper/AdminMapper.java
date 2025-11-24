06cdd5a4cf4b68cf0a820b02be288b0ad540edbd
seunghyun <jsh3990@gmail.com>
2025-11-24 17:32:31 +0900

    매출 지점별로

--- src/main/java/com/miniproject/cafe/Mapper/AdminMapper.java
+++ src/main/java/com/miniproject/cafe/Mapper/AdminMapper.java
@@ -3,10 +3,13 @@ package com.miniproject.cafe.Mapper;
 import com.miniproject.cafe.VO.AdminVO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface AdminMapper {
     void insertAdmin(AdminVO vo);
     AdminVO loginAdmin(AdminVO vo);
     int checkId(String id);
     AdminVO findById(String id);
+    List<String> getStoreList();
 }
